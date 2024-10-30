8086a4abcce66083d5bb7b9cb2c0ac9f7831e55a
Nastia <nasstyayurash@gmail.com>
2024-10-30 23:27:41 +0700

    add tasks

--- js/task-3.js
+++ js/task-3.js
@@ -1,32 +1,8 @@
 
-class StringBuilder {
-  #value;
+const nameInput = document.getElementById('name-input');
+const nameOutput = document.getElementById('name-output');
 
-  constructor(initialValue) {
-    this.#value = initialValue;
-  }
-  getValue() {
-    return this.#value;
-  }
-  padEnd(str) {
-    this.#value += str;
-
-  }
-  padStart(str) {
-    this.#value = str + this.#value;
-
-  }
-  padBoth(str) {
-this.padStart(str); 
-    this.padEnd(str); 
-  }
-}
-
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+nameInput.addEventListener('input', () => {
+  const trimmedValue = nameInput.value.trim();
+  nameOutput.textContent = trimmedValue.length > 0 ? trimmedValue : 'Anonymous';
+});
